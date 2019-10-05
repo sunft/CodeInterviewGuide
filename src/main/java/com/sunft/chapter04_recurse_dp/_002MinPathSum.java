5279fadf05597097e84d245c9a76bb8686b3502d
sunft <1535392975@qq.com>
2019-10-05 18:00:00 +0800

    换钱的最少货币数

--- src/main/java/com/sunft/chapter04_recurse_dp/_002MinPathSum.java
+++ src/main/java/com/sunft/chapter04_recurse_dp/_002MinPathSum.java
@@ -1,4 +1,68 @@
 package com.sunft.chapter04_recurse_dp;
 
+/**
+ * 矩阵的最小路径和
+ * 【题目】
+ *  给定一个矩阵m，从左上角开始每次只能向右或者向下走，最后到达右下角的位置，
+ *  路径上所有的数字累加起来就是路径和，返回所有的路径中最小的路径和。
+ *
+ */
 public class _002MinPathSum {
+
+    public int minPathSum1(int[][] m) {
+        if (m == null || m.length == 0 || m[0] == null || m[0].length == 0) {
+            return 0;
+        }
+
+        int row = m.length;
+        int col = m[0].length;
+        int[][] dp = new int[row][col];
+        dp[0][0] = m[0][0];
+        //填充第一列
+        for (int i = 1; i < row; i ++) {
+            dp[i][0] = dp[i - 1][0] + m[i][0];
+        }
+
+        //填充第一行
+        for (int j = 1; j < col; j ++) {
+            dp[0][j] = dp[0][j - 1] + m[0][j];
+        }
+
+        //填充数组的其他部分
+        for (int i = 1; i < row; i ++) {
+            for (int j = 1; j < col; j ++) {
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + m[i][j];
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+
+    public int minPathSum2(int[][] m) {
+        if (m == null || m.length == 0 || m[0] == null || m[0].length == 0) {
+            return 0;
+        }
+
+        //行数与列数较大的为more
+        int more = Math.max(m.length, m[0].length);
+        //行数与列数较小的为less
+        int less = Math.min(m.length, m[0].length);
+        //行数是不是大于等于列数
+        boolean rowmore = more == m.length;
+        //辅助数组的长度仅仅为行数与列数中的最小值
+        int[] arr = new int[less];
+        arr[0] = m[0][0];
+        for (int i = 1; i < less; i ++) {
+            arr[i] = arr[i-1] + (rowmore ? m[0][i] : m[i][0]);
+        }
+
+        for (int i = 1; i < more; i ++) {
+            arr[0] = arr[0] + (rowmore ? m[i][0] : m[0][i]);
+            for (int j = 1; j < less; j ++) {
+                arr[j] = Math.min(arr[j - 1], arr[j]) + (rowmore ? m[i][j] : m[j][i]);
+            }
+        }
+        return arr[less - 1];
+
+    }
+
 }
--- src/main/java/com/sunft/chapter04_recurse_dp/_003MinCoins.java
+++ src/main/java/com/sunft/chapter04_recurse_dp/_003MinCoins.java
@@ -1,4 +1,135 @@
 package com.sunft.chapter04_recurse_dp;
 
+/**
+ * 换钱的最少货币数
+ *
+ * 【题目】
+ *  给定数组arr，arr中所有的值都为正数且不重复。每个值代表一种面值的货币，
+ *  每种面值的货币可以使用任意张，再给定一个整数aim代表要找的钱数，求组成
+ *  aim的最少货币数。
+ *  【补充题目】
+ *   给定数组arr，arr中所有的值都为正数。每个值仅代表一张钱的面值，再给定
+ *   一个整数aim代表要找的钱数，求组成aim的最少货币数。
+ *
+ */
 public class _003MinCoins {
+
+    public int minCoins1(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return -1;
+        }
+
+        int n = arr.length;
+        int max = Integer.MAX_VALUE;
+        int[][] dp = new int[n][aim + 1];
+        for (int j = 1; j <= aim; j ++) {
+            dp[0][j] = max;
+            if (j - arr[0] >= 0 && dp[0][j -arr[0]] != max) {
+                dp[0][j] = dp[0][j - arr[0]] + 1;
+            }
+        }
+        int left = 0;
+        for (int i = 1; i < n; i ++) {
+            for (int j = 1; j <= aim; j ++) {
+                left = max;
+                if (j - arr[i] >= 0 && dp[i][j - arr[i]] != max) {
+                    left = dp[i][j - arr[i]] + 1;
+                }
+                dp[i][j] = Math.min(left, dp[i - 1][j]);
+            }
+        }
+
+        return dp[n - 1][aim] != max ? dp[n - 1][aim] : -1;
+    }
+
+    public int minCoins2(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return -1;
+        }
+
+        int n = arr.length;
+        int max = Integer.MAX_VALUE;
+        int[] dp = new int[aim + 1];
+        for (int j = 1; j <= aim; j ++) {
+            dp[j] = max;
+            if (j - arr[0] >= 0 && dp[j - arr[0]] != max) {
+                dp[j] = dp[j -arr[0]] + 1;
+            }
+        }
+
+        int left = 0;
+        for (int i = 1; i < n; i ++) {
+            for (int j = 1; j <= aim; j ++) {
+                left = max;
+                if (j - arr[i] >= 0 && dp[j - arr[i]] != max) {
+                    left = dp[j - arr[i]] + 1;
+                }
+                dp[j] = Math.min(left, dp[j]);
+            }
+        }
+        return dp[aim] != max ? dp[aim] : -1;
+
+    }
+
+    public int minCoins3(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return -1;
+        }
+
+        int n = arr.length;
+        int max = Integer.MAX_VALUE;
+        int[][] dp = new int[n][aim + 1];
+        for (int j = 1; j <= aim; j ++) {
+            dp[0][j] = max;
+        }
+
+        if (arr[0] <= aim) {
+            dp[0][arr[0]] = 1;
+        }
+
+        //左上角某个位置的值
+        int leftup = 0;
+        for (int i = 1; i < n; i ++) {
+            for (int j = 1; j <= aim; j ++) {
+                leftup = max;
+                if (j - arr[i] >= 0 && dp[i - 1][j -arr[i]] != max) {
+                    leftup = dp[i - 1][j - arr[i]] + 1;
+                }
+                dp[i][j] = Math.min(leftup, dp[i - 1][j]);
+            }
+        }
+        return dp[n - 1][aim] != max ? dp[n - 1][aim] : -1;
+    }
+
+    public int minCoins4(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return -1;
+        }
+
+        int n = arr.length;
+        int max = Integer.MAX_VALUE;
+        int[] dp = new int[aim + 1];
+        for (int j = 1; j <= aim; j ++) {
+            dp[j] = max;
+        }
+
+        if (arr[0] <= aim) {
+            dp[arr[0]] = 1;
+        }
+
+        int leftup = 0;//左上角某个位置的值
+
+        for (int i = 1; i < n; i ++) {
+            for (int j = 1; j > 0; j --) {
+                leftup = max;
+                if (j - arr[i] >= 0 && dp[j - arr[i]] != max) {
+                    leftup = dp[j - arr[i]] + 1;
+                }
+                dp[j] = Math.min(leftup, dp[j]);
+            }
+        }
+
+        return dp[aim] != max ? dp[aim] : -1;
+    }
+
 }
