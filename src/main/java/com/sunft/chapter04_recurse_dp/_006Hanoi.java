c0bae84d62aac21dbad7d341656feb8ecac06177
sunft <1535392975@qq.com>
2019-10-27 20:36:08 +0800

    最大子数列问题

--- src/main/java/com/sunft/chapter04_recurse_dp/_006Hanoi.java
+++ src/main/java/com/sunft/chapter04_recurse_dp/_006Hanoi.java
@@ -64,4 +64,66 @@ public class _006Hanoi {
         }
     }
 
+    /**
+     * 进阶递归的方式
+     * @param arr
+     * @return
+     */
+    public int step1(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        
+        return process(arr, arr.length - 1, 1, 2, 3);
+    }
+
+    private int process(int[] arr, int i, int from, int mid, int to) {
+        if (i == -1) {
+            return 0;
+        }
+
+        if (arr[i] != from && arr[i] != to) {
+            return -1;
+        }
+
+        if (arr[i] == from) {
+            return process(arr, i - 1, from, to, mid);
+        } else {
+            int rest = process(arr, i - 1, mid, from, to);
+            if (rest == -1) {
+                return -1;
+            }
+            return (1 << i) + rest;
+        }
+    }
+
+    public int step2(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+
+        int from = 1;
+        int mid = 2;
+        int to = 3;
+        int i = arr.length - 1;
+        int res = 0;
+        int tmp = 0;
+        while (i >= 0) {
+            if (arr[i] != from && arr[i] != to) {
+                return -1;
+            }
+            if (arr[i] == to) {
+                res += 1<<i;
+                tmp = from;
+                from = mid;
+            } else {
+                tmp = to;
+                to = mid;
+            }
+            mid = tmp;
+            i--;
+        }
+        return res;
+    }
+
 }
